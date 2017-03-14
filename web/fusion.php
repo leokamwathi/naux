dc7c7af514ca6c56f913b28f8fbb329de27e4090
leokamwathi <leo.kamwathi@gmail.com>
2017-03-14 03:00:50 +0300

    foo
    foo

--- web/fusion.php
+++ web/fusion.php
@@ -5,10 +5,20 @@ $client = new Google_Client();
 $client->setApplicationName("Anime");
 $client->setDeveloperKey("AIzaSyA9VECd6_keyzCsId7hxxU-bcVAijMv6SM");
 
-$service = new Google_Service_Books($client);
-$optParams = array('filter' => 'free-ebooks');
-$results = $service->volumes->listVolumes('Henry David Thoreau', $optParams);
+$service = new Google_FusiontablesService($client);
 
-foreach ($results as $item) {
-  echo $item['volumeInfo']['title'], "<br /> \n";
-}
+$selectQuery = "SELECT 'title', 'anime_type', 'episodes', 'synopsis', 'mal_score', 'mal_image','mal_id' FROM 1409399 ORDER BY mal_score DESC LIMIT 100";
+echo "<pre>";
+print_r($service->query->sql($selectQuery));
+echo "</pre>";
+
+
+
+
+
+
+
+
+
+
+
