080d5e14b16ba80cfd455fde62d3f7a04fe3d792
leokamwathi <leo.kamwathi@gmail.com>
2017-03-16 02:35:38 +0300

    decode
    er

--- web/port.php
+++ web/port.php
@@ -25,7 +25,67 @@ $message = $fb->entry[0]->messaging[0]->message->text;
 //if ($message > 0){
 if (isset($message) && $message != '') {
 
+if ($message == 'json') {
+$myJson = 'recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for first bubble",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "Payload for second bubble",
+            }]
+          }]
+        }
+      }
+    }
+  }; '
 
+  $token = "EAAN5JK8Gx7sBAGCZB5YulfJl4eoUCXGZABOm1oGRFH4kHubnxeANv8ZCVRQymrxqm0BEpzdULKWKhaBi5qXSbxZBrWhKud2U3ZAsBi1e8y3xCuKUMz9UF5XWRM8O9moGoIidAsUyCr3FLKjlXd0Q2WC70x6vmIZBwajPKXbxKU7AZDZD";
+
+  $data = json_decode($myJson);
+
+$options = array(
+'http' => array(
+'method' => 'POST',
+'content' => json_encode($data),
+'header' => "Content-Type: application/json\n"
+)
+
+);
+$context = stream_context_create($options);
+
+
+$reply = file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
+  
+  }else{
 $data = file_get_contents("php://input");
 
 $options = array(
@@ -65,6 +125,7 @@ $context = stream_context_create($options);
 
 
 $reply = file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
+}
 }
 }
 } catch (Exception $e) {
