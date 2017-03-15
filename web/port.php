fd0d45d65ea267378521d3d1fbc948466a4e573e
leokamwathi <leo.kamwathi@gmail.com>
2017-03-15 23:22:34 +0300

    ultimate
    xp

--- web/port.php
+++ web/port.php
@@ -23,7 +23,7 @@ $data = file_get_contents("php://input");
 $options = array(
 'http' => array(
 'method' => 'POST',
-'content' => $data ,
+'content' => json_encode($data),
 'header' => "Content-Type: application/json\n"
 )
 
@@ -32,6 +32,42 @@ $context = stream_context_create($options);
 
 //$fb = json_decode($data);
 
-	$result = file_get_contents("http://fbbot.synax-solutions.com/bot.aspx?result=$data", false, $context);
-	print_r($result);
+	$message = file_get_contents("http://fbbot.synax-solutions.com/bot.aspx?result=$data", false, $context);
+	//print_r($result);
+	
+	
+	if (isset($message) && $message != '') {
+
+$token = "EAAN5JK8Gx7sBAGCZB5YulfJl4eoUCXGZABOm1oGRFH4kHubnxeANv8ZCVRQymrxqm0BEpzdULKWKhaBi5qXSbxZBrWhKud2U3ZAsBi1e8y3xCuKUMz9UF5XWRM8O9moGoIidAsUyCr3FLKjlXd0Q2WC70x6vmIZBwajPKXbxKU7AZDZD";
+
+$data = array(
+'recipient' => array('id'=> $rid),
+'message' => array('text'=> $message)
+);
+
+$options = array(
+'http' => array(
+'method' => 'POST',
+'content' => json_encode($data),
+'header' => "Content-Type: application/json\n"
+)
+
+);
+$context = stream_context_create($options);
+
+
+$reply = file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
+}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
