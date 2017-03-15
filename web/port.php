c0be52e6b095d6f3bd4594213068826ca61a7ac5
leokamwathi <leo.kamwathi@gmail.com>
2017-03-15 17:12:32 +0300

    a duzzy
    yes

--- web/port.php
+++ web/port.php
@@ -1,10 +1,15 @@
 <?php
-
+/*
 $ch = curl_init('http://fbbot.synax-solutions.com/bot.aspx');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
 curl_setopt($ch, CURLOPT_POSTFIELDS, $_POST);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $result = curl_exec($ch);
+*/
+
+
+
+$result = file_get_contents("http://fbbot.synax-solutions.com/bot.aspx", false, file_get_contents("php://input"));
 
 print_r($result);
