8cd522d90798e74cfd2fafc56e5af7c5eeae2768
leokamwathi <leo.kamwathi@gmail.com>
2017-03-15 19:36:27 +0300

    json
    jj

--- web/port.php
+++ web/port.php
@@ -12,6 +12,8 @@ $result = curl_exec($ch);
 if (isset($_GET["hub_challenge"]) && $_GET["hub_challenge"] != '') {
 	print_r($_GET["hub_challenge"]);
 }else{
-	$result = file_get_contents("http://fbbot.synax-solutions.com/bot.aspx", false, file_get_contents("php://input"));
+$content = file_get_contents("php://input");
+$fb = json_decode($content);
+	$result = file_get_contents("http://fbbot.synax-solutions.com/bot.aspx?result=$fb", false, $content);
 	print_r($result);
 }
