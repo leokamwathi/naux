80abb549e5a813fd4ba47f5752006caeab383e59
leokamwathi <leo.kamwathi@gmail.com>
2017-03-17 04:45:48 +0300

    yes work
    work

--- web/dbtest.php
+++ web/dbtest.php
@@ -7,6 +7,19 @@ function pg_connection_string_from_database_url() {
 }
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+$createTable = 'CREATE TABLE IF NOT EXISTS Json_Messages (
+                        json text NOT NULL UNIQUE
+                     )';
+$result = pg_query($pg_conn, $createTable );
+
+$insertData = '{"object":"page","entry":[{"id":"763933067090623","time":1489656298161,"messaging":[{"sender":{"id":"1486644564679609"},"recipient":{"id":"763933067090623"},"timestamp":1489656298087,"message":{"mid":"mid.$cAAK2yxk7oTRhB7SCZ1a1m5n8K6Fr","seq":4271,"text":"rift"}}]}]}';
+
+$insertQuery = "INSERT INTO Json_Messages (json)
+    VALUES ('$insertData');";
+$result = pg_query($pg_conn, $insertQuery );
+
+
 # Now let's use the connection for something silly just to prove it works:
 $result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 print "<pre>\n";
