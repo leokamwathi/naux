b6848b9ad8091d90e5f265c96df1f1fd9dd8d5ce
leokamwathi <leo.kamwathi@gmail.com>
2017-03-24 22:23:38 +0300

    new
    new

--- web/createDBtest.php
+++ web/createDBtest.php
@@ -1,4 +1,18 @@
 <?php
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # This function reads your DATABASE_URL config var and returns a connection
 # string suitable for pg_connect. Put this in your app.
 function pg_connection_string_from_database_url() {
@@ -8,9 +22,30 @@ function pg_connection_string_from_database_url() {
 # Here we establish the connection. Yes, that's all.
 $pg_conn = pg_connect(pg_connection_string_from_database_url());
 
-/*
-$createTable = 'CREATE TABLE IF NOT EXISTS Json_Messages (
-                        json text NOT NULL UNIQUE
+
+$createTable = 'CREATE TABLE IF NOT EXISTS jobsDBtest (
+                        pageID text NOT NULL UNIQUE,
+						userID text NOT NULL UNIQUE,
+						userType text NOT NULL UNIQUE,
+						Name text NOT NULL UNIQUE,
+						status text NOT NULL UNIQUE,
+						waitingFor text NOT NULL UNIQUE,
+						email text NOT NULL UNIQUE,
+						location text NOT NULL UNIQUE,
+						city text NOT NULL UNIQUE,
+						country text NOT NULL UNIQUE,
+						geo_location text NOT NULL UNIQUE,
+						job text NOT NULL UNIQUE,
+						description text NOT NULL UNIQUE,
+						experience text NOT NULL UNIQUE,
+						qualification text NOT NULL UNIQUE,
+						expiry_date text NOT NULL UNIQUE,
+						last_notification text NOT NULL UNIQUE,
+						active text NOT NULL UNIQUE,
+						join_date text NOT NULL UNIQUE,
+						paid text NOT NULL UNIQUE,
+						amountPaid text NOT NULL UNIQUE,
+						paymentID text NOT NULL UNIQUE
                      )';
 $result = pg_query($pg_conn, $createTable );
 
@@ -20,7 +55,7 @@ $insertQuery = "INSERT INTO Json_Messages (json)
     VALUES ('$insertData');";
 $result = pg_query($pg_conn, $insertQuery );
 
-*/
+
 # Now let's use the connection for something silly just to prove it works:
 //$result = pg_query($pg_conn, "SELECT relname FROM pg_stat_user_tables WHERE schemaname='public'");
 
@@ -34,4 +69,49 @@ if (!pg_num_rows($result)) {
   while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
 }
 print "\n";
+
+
+
+
+
+
+/*
+
+try {
+$dbuser = 'postgres';
+$dbpass = 'abc123';
+$host = 'localhost';
+$dbname='postgres';
+
+$connec = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+}catch (PDOException $e) {
+echo "Error : " . $e->getMessage() . "<br/>";
+die();
+}
+$sql = 'SELECT fname, lname, country FROM user_details ORDER BY country';
+foreach ($connec->query($sql) as $row) 
+{
+print $row['fname'] . " ";
+print $row['lname'] . "-->";
+print $row['country'] . "<br>";
+}
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- web/port.php
+++ web/port.php
@@ -1,4 +1,136 @@
 <?php
+
+
+//====================================================================================================================//
+/////////////////Messages
+
+$status_new = '  
+"{recipient":{
+    "id":"'.$sid.'"
+  },
+  "message":{
+    "text":"Hi, Welcome to the job bot. I will help you find a job or find job applicants. What do you want to do?",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Find Job",
+        "payload":"findJob"
+      },
+      {
+        "content_type":"text",
+        "title":"Post Job",
+        "payload":"postJob"
+      }
+    ]
+   }
+  }';
+
+$status_location = '{
+  "recipient":{
+    "id":"'.$sid.'"
+  },
+  "message":{
+    "text":"Please enter your job location : (city,country) eg. Nairobi, Kenya or use your current location.",
+    "quick_replies":[
+      {
+        "content_type":"location",
+      }
+    ]
+  }
+}';
+
+$status_job = '  
+"{recipient":{
+    "id":"'.$sid.'"
+  },
+  "message":{
+    "text":"Hi, What kind of job are you looking for (Just one Job)? eg. Part time, Accountant, Web Designer,Chef, Sales Person, Programmer, House Help.",
+   }
+  }';
+
+$status_exp = '  
+"{recipient":{
+    "id":"'.$sid.'"
+  },
+  "message":{
+    "text":"How many years have you worked at this job?",
+    "quick_replies":[
+	   {
+        "content_type":"text",
+        "title":"First Time",
+        "payload":"first"
+      },
+      {
+        "content_type":"text",
+        "title":"Under 1 year",
+        "payload":"under_1"
+      },
+      {
+        "content_type":"text",
+        "title":"1 to 3 years",
+        "payload":"1_to_3"
+      },
+      {
+        "content_type":"text",
+        "title":"4 to 8 years",
+        "payload":"4_to_8"
+      },
+      {
+        "content_type":"text",
+        "title":"9 years and above",
+        "payload":"Over_9"
+      }
+    ]
+   }
+  }';
+
+$status_qualifications = '  
+"{recipient":{
+    "id":"'.$sid.'"
+  },
+  "message":{
+    "text":"What is your job qualification Level?",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Self Taught",
+        "payload":"selfTaught"
+      },
+      {
+        "content_type":"text",
+        "title":"Certificate",
+        "payload":"certificate"
+      },
+      {
+        "content_type":"text",
+        "title":"Collage Diploma",
+        "payload":"collage_diploma"
+      },
+      {
+        "content_type":"text",
+        "title":"University Degree",
+        "payload":"university_degree"
+      },
+      {
+        "content_type":"text",
+        "title":"Masters Degree",
+        "payload":"masters_degree"
+      }
+    ]
+   }
+  }';
+
+$status_about = '  
+"{recipient":{
+    "id":"'.$sid.'"
+  },
+  "message":{
+    "text":"Hi, Tell us about yourself and the job you are looking for?",
+   }
+  }';
+
+//====================================================================================================================//
+//////Hub Challenge
 /*
 $ch = curl_init('http://fbbot.synax-solutions.com/bot.aspx');
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
@@ -10,171 +142,123 @@ $result = curl_exec($ch);
 
 
 if (isset($_GET["hub_challenge"]) && $_GET["hub_challenge"] != '') {
-	print_r($_GET["hub_challenge"]);
+    print_r($_GET["hub_challenge"]);
+
+
 }else{
+    //================================================================================================================
+    /////// Read Input set fb variables
 
+    try {
 
-try {
+        $data = file_get_contents("php://input");
 
-$data = file_get_contents("php://input");
-
-$options = array(
-'http' => array(
-'method' => 'POST',
-'content' => $data,
-'header' => "Content-Type: application/json\n"
-)
-
-);
-$context = stream_context_create($options);
-
-//$fb = json_decode($data);
-
-
-//================================================================================================================
-
-function pg_connection_string_from_database_url() {
-  extract(parse_url($_ENV["DATABASE_URL"]));
-  return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
-}
-# Here we establish the connection. Yes, that's all.
-$pg_conn = pg_connect(pg_connection_string_from_database_url());
-
-$insertQuery = "INSERT INTO Json_Messages (json)
-    VALUES ('$data')";
-$result = pg_query($pg_conn, $insertQuery );
+        //$fb = json_decode($data);
 
 
 
-//================================================================================================================
+        /// PAYLOAD Format
+        ///{"object":"page","entry":[{"id":"763933067090623","time":1490364301410,"messaging":[{"recipient":{"id":"763933067090623"},"timestamp":1490364301410,"sender":{"id":"1486644564679609"},"postback":{"payload":"Payload"}}]}]}
 
-$messageHost = file_get_contents("http://fbbot.synax-solutions.com/bot.aspx?result=$data", false, $context);
-//print_r($result);
+        $fb = json_decode($data);
 
+        $pid = $fb->entry[0]->id;
+        $sid = $fb->entry[0]->messaging[0]->sender->id;
+        $message = $fb->entry[0]->messaging[0]->message->text;
+        $payload = $fb->entry[0]->messaging[0]->postback->payload;
 
-$fb = json_decode($data);
-
-//$sid = $fb->entry[0]->id;
-$rid = $fb->entry[0]->messaging[0]->sender->id;
-$message = $fb->entry[0]->messaging[0]->message->text;
-
-
-//if ($message > 0){
-if (isset($message) && $message != '') {
-
-if ($message == 'json') {
-// work work work.... 
-//$data = '{"recipient": {
-//      "id": "'.$rid.'"
-//    },
-//    "message": {"text" : "I am alive people"}}';
-
-$myJson = '{"recipient": {
-      "id": "'.$rid.'"
-    },
-    "message": {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "rift",
-            "subtitle": "Next-generation virtual reality",
-            "item_url": "https://www.oculus.com/en-us/rift/",               
-            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-            "buttons": [{
-              "type": "web_url",
-              "url": "https://www.oculus.com/en-us/rift/",
-              "title": "Open Web URL"
-            }, {
-              "type": "postback",
-              "title": "Callback",
-             "payload": "Payload"
-            }]
-          }]
+         if (isset($message) && $message != '') {
+             $message = "Payload ".$payload;
+         }
+        //===================================================================================================
+        // database connections
+        function pg_connection_string_from_database_url() {
+            extract(parse_url($_ENV["DATABASE_URL"]));
+            return "user=$user password=$pass host=$host dbname=" . substr($path, 1); # <- you may want to add sslmode=require there too
         }
-      }
+        # Here we establish the connection. Yes, that's all.
+        $pg_conn = pg_connect(pg_connection_string_from_database_url());
+
+        $checkStatusQuery = "SELECT * from jobsDB where pageID ='$pid' and userID='$sid'";
+
+        //$insertQuery = "INSERT INTO Json_Messages (json) VALUES ('$data')";
+
+        $status = pg_query($pg_conn, $checkStatusQuery );
+        $isNewUser = true;
+
+        if (!pg_num_rows($result)) {
+            $status = "New";
+            //Add intial Data
+        } else {
+            //print "Json Messages in your database:\n";
+            //while ($row = pg_fetch_row($result)) { print("- $row[0]\n"); }
+            while ($row = pg_fetch_row($result)) {
+                //$row = pg_fetch_row($result)
+                $status = $row['status'];
+            }
+        }
+
+        //====================================================================================================
+
+        switch ($message) {
+            case "new":
+                $reply = $status_new;
+                break;
+            case "location":
+                $reply = $status_location;
+                break;
+            case "job":
+                $reply = $status_job;
+                break;
+            case "exp":
+                $reply = $status_exp;
+                break;
+            case "qualification":
+                $reply = $status_qualification;
+                break;
+            case "about":
+                $reply = $status_about;
+                break;
+            default:
+                $reply = $message;
+                }
+
+        //====================================================================================================
+
+         if (isset($reply) && $reply != '') {
+
+                    $token = "EAAN5JK8Gx7sBAGCZB5YulfJl4eoUCXGZABOm1oGRFH4kHubnxeANv8ZCVRQymrxqm0BEpzdULKWKhaBi5qXSbxZBrWhKud2U3ZAsBi1e8y3xCuKUMz9UF5XWRM8O9moGoIidAsUyCr3FLKjlXd0Q2WC70x6vmIZBwajPKXbxKU7AZDZD";
+
+                    $data = array(
+                        'recipient' => array('id'=> $sid),
+                        'message' => array('text'=> $reply)
+                    );
+
+                    $options = array(
+                        'http' => array(
+                        'method' => 'POST',
+                        'content' => json_encode($data),
+                        'header' => "Content-Type: application/json\n"
+                    )
+
+                    );
+                    $context = stream_context_create($options);
+
+                    $reply = file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
+                }
+    } catch (Exception $e) {
+        // Handle exception
     }
-  }';
-  
-  
-  /*
-  
-  , {
-            "title": "touch",
-            "subtitle": "Your Hands, Now in VR",
-            "item_url": "https://www.oculus.com/en-us/touch/",               
-            "image_url": "http://messengerdemo.parseapp.com/img/touch.png",
-            "buttons": [{
-              "type": "web_url",
-              "url": "https://www.oculus.com/en-us/touch/",
-              "title": "Open Web URL"
-            }, {
-              "type": "postback",
-              "title": "Call Postback",
-              "payload": "Payload for second bubble"
-            }]
-          }
-  
-  */
-
-  $token = "EAAN5JK8Gx7sBAGCZB5YulfJl4eoUCXGZABOm1oGRFH4kHubnxeANv8ZCVRQymrxqm0BEpzdULKWKhaBi5qXSbxZBrWhKud2U3ZAsBi1e8y3xCuKUMz9UF5XWRM8O9moGoIidAsUyCr3FLKjlXd0Q2WC70x6vmIZBwajPKXbxKU7AZDZD";
-
-  //$data = json_decode($myJson);
-
-$options = array(
-'http' => array(
-'method' => 'POST',
-'content' => $myJson ,
-'header' => "Content-Type: application/json\n"
-)
-
-);
-$context = stream_context_create($options);
 
 
-$reply = file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
-  
-  }else{
-	
-	
-	if (isset($messageHost) && $messageHost != '') {
-
-$token = "EAAN5JK8Gx7sBAGCZB5YulfJl4eoUCXGZABOm1oGRFH4kHubnxeANv8ZCVRQymrxqm0BEpzdULKWKhaBi5qXSbxZBrWhKud2U3ZAsBi1e8y3xCuKUMz9UF5XWRM8O9moGoIidAsUyCr3FLKjlXd0Q2WC70x6vmIZBwajPKXbxKU7AZDZD";
-
-$data = array(
-'recipient' => array('id'=> $rid),
-'message' => array('text'=> $messageHost)
-);
-
-$options = array(
-'http' => array(
-'method' => 'POST',
-'content' => json_encode($data),
-'header' => "Content-Type: application/json\n"
-)
-
-);
-$context = stream_context_create($options);
 
 
-$reply = file_get_contents("https://graph.facebook.com/v2.6/me/messages?access_token=$token", false, $context);
-}
-}
-}
-} catch (Exception $e) {
-    // Handle exception
-}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
+
 }
