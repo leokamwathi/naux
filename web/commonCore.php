e3fcb2426873c49e35b394cdf77403b8dca82e3a
leokamwathi <leo.kamwathi@gmail.com>
2017-04-30 22:02:27 +0300

    lots of doo
    sdfsdf

--- web/commonCore.php
+++ web/commonCore.php
@@ -592,7 +592,6 @@ function nextStatus($userStatus)
                                 $GLOBALS['smsg'] = $msg;
                                 $msg = trim(preg_replace('/\s+/', ' ', $msg));
                                 $msg  = str_replace("\n", "?*?#*^n", $msg);
-                            	$msg  = str_replace("\\", "\\\\\\", $msg);
                                 $msg  = str_replace("?*?#*^n", "\n", $msg);
                                 //$msg = jsonFixer($msg);
                                 $options = array(
