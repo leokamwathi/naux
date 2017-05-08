7687725a757a5a1f33d441d6d75b90a76c1a4240
leokamwathi <leo.kamwathi@gmail.com>
2017-05-08 14:58:08 +0300

    ender
    end this

--- web/searchjob.php
+++ web/searchjob.php
@@ -7,7 +7,7 @@ require_once 'databaseCore.php';
 
 $GLOBALS['token'] = $_ENV["techware_fb_token"];
 
-$query = "SELECT * from ".$GLOBALS['dbTable']." where userType = 'Find-Job' AND isNotification = 'YES' AND status = 'info'";
+$query = "SELECT * from ".$GLOBALS['dbTable']." where userType = 'Find-Job' AND isNotification = 'YES'";
 $results = excuteQuery($query);
 $FindJobs = 0;
 $PostJobs = 0;
@@ -25,18 +25,19 @@ if (pg_num_rows($results)) {
         if(isset($GLOBALS['username']) && $GLOBALS['username']!= '' && $row['status'] = 'info'){
             $FindJobs = $FindJobs+1;
             //$users = $users +1 ;
-        //    searchJobs(0);
+            searchJobs(0);
             //$GLOBALS['sid'] = $row['userid'];
-            //$GLOBALS['sid'] = $_ENV['my_userID'];
-            //sendMessage($GLOBALS['status_search_results']);
+            $GLOBALS['sid'] = $_ENV['my_userID'];
+            sendMessage(basicReply('Hi '.$GLOBALS['username'].', Below are the jobs matching your description that I have found.'));
+            sendMessage($GLOBALS['status_search_results']);
         }else{
             //$users = $users +1 ;
             //sendMessage($GLOBALS['status_options_find-job']);
             //if($GLOBALS['sid'] == $_ENV['my_userID']){
             //$GLOBALS['sid'] = $row['userid'];
-            //$GLOBALS['sid'] = $_ENV['my_userID'];
-        //    sendMessage(basicReply('You need to complete your job profile before you can get a job notification.'));
-        //    sendReply($row['status']);
+            $GLOBALS['sid'] = $_ENV['my_userID'];
+            sendMessage(basicReply('Hi '.$GLOBALS['username'].',You need to complete your job profile before you can get a job notification.'));
+            //sendReply($row['status']);
             //}
         }
     }
